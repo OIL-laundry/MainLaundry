b90da6165707e5163b7954a7750080c3d3f9da24
liadn7 <liadnagi@gmail.com>
2021-12-26 12:32:40 +0200

    fix layout

--- app/src/main/java/com/example/oil_laundry/OILCalendar.java
+++ app/src/main/java/com/example/oil_laundry/OILCalendar.java
@@ -10,37 +10,7 @@ public class OILCalendar {
         this.hour=0;
     }
 
-/*    public Integer getDay(int dayOfMonth) {
-        return day;
-    }
 
-    public void setDay(Integer day) {
-        this.day = day;
-    }
-
-    public Integer getMonth(int i) {
-        return month;
-    }
-
-    public void setMonth(Integer month) {
-        this.month = month;
-    }
-
-    public Integer getYear(int year) {
-        return this.year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Integer getHour() {
-        return hour;
-    }
-
-    public void setHour(Integer hour) {
-        this.hour = hour;
-    }*/
 
     public String toString(){
         return (day+"/"+month+"/"+year+" "+hour+":00");
